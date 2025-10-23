18d3856928fffdfe0a899b713e991c963285823f
Excaliose777 <alaminxab@gmail.com>
2025-10-23 13:54:35 +0100

    Readme

--- src/components/AudioUpload.jsx
+++ src/components/AudioUpload.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef } from "react";
 
-const AudioUpload = ({ onAudioLoad, currentAudioUrl }) => {
+const gigiAudioUpload = ({ onAudioLoad, currentAudioUrl }) => {
   const [isUploading, setIsUploading] = useState(false);
   const [error, setError] = useState(null);
   const fileInputRef = useRef(null);
